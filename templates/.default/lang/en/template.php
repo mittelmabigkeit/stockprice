c4cc25452901937e735762dc1c5e049dc9c2ba36
mittelmabigkeit <goodnazi1488@gmail.com>
2026-01-20 14:47:45 +0300

    Create template.php

---
+++ templates/.default/lang/en/template.php
@@ -0,0 +1,6 @@
+<?
+$MESS ['MOSBIRZHA'] = "Moscow Exchange";
+$MESS ['UPDATED'] = "Updated";
+$MESS ['TICKER'] = "Ticker";
+$MESS ['TEXT'] = 'The shares of "ROSINTER RESTAURANTS HOLDING (PJSC)" are traded on the Moscow Exchange (ticker ROST) and are included in the third-level quotation list.';
+?>
